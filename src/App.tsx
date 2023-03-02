bc43c2c6d6f172e1de348a600f2a310503e01b60
BadMonkeyDev <arsenii.aivazian@gmail.com>
2023-03-02 19:15:55 +0200

    added absolute paths, fixed configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from "./widgets/Header";
 import AppRouter from "./provider/router/component/AppRouter";
+import Footer from "./widgets/Footer";
 
 const App = () => {
     return (
         <div className={'App'}>
             <Header/>
             <AppRouter />
+            <Footer />
         </div>
     );
 };
