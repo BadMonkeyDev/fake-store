ba073be27e971bd16b91f6bd659868e0f0c772bb
BadMonkeyDev <arsenii.aivazian@gmail.com>
2023-03-03 18:15:38 +0200

    added categories slice

---
+++ src/store/reducers/categoriesSlice.ts
@@ -0,0 +1,49 @@
+import {createAsyncThunk, createSelector, createSlice} from '@reduxjs/toolkit';
+import {fetchData, PATHS} from 'http/index';
+import {RootState} from "../store";
+
+export const fetchCategories = createAsyncThunk('products/fetchCategories', async () => {
+    return await fetchData(PATHS.CATEGORIES);
+});
+
+export type CategoriesDataType = string[] | null
+
+export interface CategoriesState {
+    data: CategoriesDataType;
+    loading: boolean;
+    error: string | null;
+}
+
+const initialState: CategoriesState = {
+    data: [],
+    loading: false,
+    error: null,
+};
+
+export const categoriesSlice = createSlice({
+    name: 'categories',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchCategories.pending, (state) => {
+                state.loading = true;
+                state.error = null;
+            })
+            .addCase(fetchCategories.fulfilled, (state, action) => {
+                state.loading = false;
+                state.data = action.payload;
+            })
+            .addCase(fetchCategories.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.error.message ?? 'Failed to fetch categories';
+            });
+    },
+});
+
+export const selectCategories = createSelector(
+    (state: RootState) => state.categories.data,
+    (categories: CategoriesDataType) => categories
+);
+
+export default categoriesSlice.reducer;
